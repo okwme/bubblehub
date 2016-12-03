bb9cf6a2bd0a1eea64fe6345f92ec9ffa9b0e692
evvvritt <evv.info@gmail.com>
2016-12-03 22:33:45 +0100

    toy click / check in

--- src/components/VR.vue
+++ src/components/VR.vue
@@ -12,8 +12,8 @@
       <!--<a-image position="0 -.2 5" src="#highlight1" rotation="-90 0 0" scale="30 30 30"></a-image>-->
 
       <!-- Objects -->
-      <a-entity toy-color position="0 2 -3" obj-model="obj: #bus-obj" rotation="-3 -45 0">
-        <a-animation attribute="rotation"
+      <a-entity toy-color check-in position="0 2 -3" obj-model="obj: #plane-obj" rotation="-3 -45 0">
+        <a-animation v-if="animOn" attribute="rotation"
                   direction="normal"
                   dur="20000"
                   fill="reverse"
@@ -21,8 +21,13 @@
                   easing="linear"
                   repeat="indefinite"></a-animation>
       </a-entity>
+      
+      <!-- camera / cursor -->
+      <a-entity camera="userHeight: 1.6" look-controls>
+        <a-cursor></a-cursor>
+      </a-entity>
 
-      <!-- Background -->
+      <!-- Background / loc.photo -->
       <a-sky :src="loc.photo"></a-sky>
 
     </a-scene>
@@ -36,13 +41,24 @@ export default{
   props: ['loc'],
   data () {
     return {
+      animOn: true
     }
   },
   created () {
-    console.log(AFRAME.version)
+    // console.log(AFRAME.version)
     AFRAME.registerComponent('toy-color', {
       init: function () {
         this.el.setAttribute('material', 'color:red')
+      }
+    })
+    AFRAME.registerComponent('check-in', {
+      init: function () {
+        const COLORS = ['red', 'green', 'blue']
+        this.el.addEventListener('click', function () {
+          const randomIndex = Math.floor(Math.random() * COLORS.length)
+          this.setAttribute('material', 'color', COLORS[randomIndex])
+          console.log('I was clicked!')
+        })
       }
     })
   }
